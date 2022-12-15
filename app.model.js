876561e21720da26ec8586a503eca38fa461790f
ThacHungNguyen <nthachung@gmail.com>
2022-12-15 16:23:13 +0700

    update

---
+++ app.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const appSchema = mongoose.Schema({
+  name: { type: String, default: "" },
+  PID: { type: String, default: "" },
+  pathFull: { type: String, default: "" },
+  MD5: { type: String, default: "" },
+});
+
+const appModel = mongoose.model("appModel", appSchema);
+export default appModel;
---
+++ index.js
@@ -0,0 +1,58 @@
+import express from "express";
+import mongoose from "mongoose";
+
+import child_process from "child_process";
+import psList from "ps-list";
+import currentProcesses from "current-processes";
+import fs from "fs";
+const app = express();
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect("mongodb://localhost:27017/procman")
+  .then(() => console.log("database Connected!"));
+
+const processNameList = new Promise((resolve, reject) => {
+  let command = `osascript -e 'tell application "System Events" to return name of processes whose background only is false'`;
+  child_process.exec(command, (err, stdout) => {
+    if (err) throw err;
+    const processList = stdout.split(", ");
+    resolve(processList);
+  });
+});
+
+const processes = new Promise((resolve, reject) => {
+  currentProcesses.get(function (err, processes) {
+    resolve(processes);
+  });
+});
+
+app.get("/procman", (req, res) => {
+  Promise.all([processNameList, processes]).then(([processNameList, processes]) => {
+    const response = [];
+    for (let name of processNameList) {
+      for (let process of processes) {
+        name = name.replace(/(\r\n|\n|\r)/gm, "");
+        if (process.name === name) {
+          response.push(process);
+        }
+      }
+    }
+    res.send(response);
+  });
+});
+
+app.post("/killapp", (req, res) => {
+  const pid = req.body.pid;
+  if (!pid) res.send("No pid");
+  const killAppActin = new Promise((res, rej) => {
+    res(process.kill(pid));
+  });
+  killAppActin.then(() => res.send("kill app success")).catch();
+});
+
+const port = process.env.port || 9527;
+
+app.listen({ port }, () => {
+  console.log(` Server ready at http://localhost:${port}`);
+});
